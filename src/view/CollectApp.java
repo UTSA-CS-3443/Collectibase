3aacd62aed9e22f584887f79832f8fc515aed8f4
Travis Lawson <Travis.Lawson91@gmail.com>
2017-10-17 13:43:02 -0500

    cleaned up code for CollectApp.java

--- src/view/CollectApp.java
+++ src/view/CollectApp.java
@@ -2,34 +2,17 @@ package view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-//import javafx.scene.layout.BorderPane; currently not used
-
 
 public class CollectApp extends Application {
-	public Stage stage;
-	public AnchorPane layout;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			this.stage = primaryStage;
-			this.stage.setTitle("Collectibase Test");
-			initLayout();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	public void initLayout() {
-		try {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation( CollectApp.class.getResource("/CollectMainMenu.fxml"));
-			this.layout = (AnchorPane) loader.load();
-			
-			Scene scene = new Scene( layout );
-			this.stage.setScene( scene );
-			this.stage.show();
-		}catch (Exception e) {
+			Parent root = FXMLLoader.load(getClass().getResource("/CollectMainMenu.fxml"));
+			primaryStage.setScene(new Scene(root, 500, 575));
+			primaryStage.show();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
