79372f84bc0c828c608d48c8bed7d406498ceff4
Travis Lawson <Travis.Lawson91@gmail.com>
2017-10-21 11:58:05 -0500

    Storage class is used to hold the ArrayList for specific collections

---
+++ src/model/Storage.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.ArrayList;
+
+/*
+ * used to hold the lists of specific collections. 
+ */
+public class Storage {
+	public ArrayList<VHSCollection> vhs = new ArrayList<VHSCollection>();
+}
