31e3eac6cc2d54a7af02a57c9bec0205a02fe08a
Travis Lawson <Travis.Lawson91@gmail.com>
2017-10-24 11:49:14 -0500

    made the ArrayList static

--- src/model/Storage.java
+++ src/model/Storage.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
  * used to hold the lists of specific collections. 
  */
 public class Storage {
-	public ArrayList<VHSCollection> vhs = new ArrayList<VHSCollection>();
-	public ArrayList<DVDCollection> dvd = new ArrayList<DVDCollection>();
+	// ArrayList set as static because there will be only one of each collection.
+	// Array Lists can be accessed by Storage."list type"
+	public static ArrayList<VHSCollection> vhs = new ArrayList<VHSCollection>();
+	public static ArrayList<DVDCollection> dvd = new ArrayList<DVDCollection>();
 }
