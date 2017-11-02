004706434a3a5a1e026ba82e3483220e214743b7
Travis Lawson <Travis.Lawson91@gmail.com>
2017-11-02 11:19:18 -0500

    created a VinylCollection class

---
+++ src/model/VinylCollection.java
@@ -0,0 +1,11 @@
+package model;
+
+//TODO: Travis to write for vinyl
+public class VinylCollection extends MusicCollection {
+
+	public VinylCollection(String artist, String albumName, String year) {
+		super(artist, albumName, year);
+		// TODO Auto-generated constructor stub
+	}
+
+}
