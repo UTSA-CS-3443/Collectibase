5d72d495207e65ac51c6b8418e081367a77c612d
Travis Lawson <Travis.Lawson91@gmail.com>
2017-11-02 11:24:34 -0500

    basic cd collection class

--- src/model/CDCollection.java
+++ src/model/CDCollection.java
@@ -1,6 +1,17 @@
 package model;
 
 public class CDCollection extends MusicCollection{
+
 	
+	public CDCollection(String artist, String albumName, String year, String musicFormat) {
+		super(artist, year);
+		setAlbum(albumName);
+		setMusicFormat(musicFormat);
+	}
+	
+	public CDCollection(String artist, String album) {
+		super(artist);
+		setAlbum(album);
+	}
 
 }
