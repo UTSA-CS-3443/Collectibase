82fffe2ec445808ddf526899a8ad6e3450c11dd5
Travis Lawson <Travis.Lawson91@gmail.com>
2017-11-06 10:50:17 -0600

    Made a few tweaks to Tools readInFile method. Both read and write will need more work

--- src/model/Tools.java
+++ src/model/Tools.java
@@ -1,12 +1,10 @@
 package model;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
 
 /**
  * \ Different tools used for Collectibase. Writing to file, reading form file,
@@ -24,8 +22,9 @@ public class Tools {
 			if (Storage.vhs.size() > 0) {
 				out.println("VHS");
 				for (int i = 0; i < Storage.vhs.size(); i++) {
-					out.println(Storage.vhs.get(i).getName());
-					out.println(Storage.vhs.get(i).getSleeveCondition());
+					out.println("format " + Storage.vhs.get(i).getFormat());
+					out.println("title " + Storage.vhs.get(i).getName());
+					// out.println(Storage.vhs.get(i).getSleeveCondition());
 				}
 			}
 
@@ -38,39 +37,45 @@ public class Tools {
 	}
 
 	public static void readInFile() throws IOException {
-		System.out.println("Read in File");
-		// Scanner in = null;
+		System.out.println("Tools.readInFile()");
 		String fileName = "Master.txt";
 		String line = null;
 
 		String format;
 		String title;
+		VHSCollection newVHS;
+		DVDCollection newDVD;
 
 		try {
 			FileReader fileReader = new FileReader(fileName);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 
 			while ((line = bufferedReader.readLine()) != null) {
-				if (line.contains("start")) {
-					VHSCollection newVHS = new VHSCollection("");
-					Storage.vhs.add(newVHS);
-				}
-
+				// format can be used for movies and music
+				
 				if (line.contains("format")) {
 					String[] tokens = line.split(" ");
 					format = tokens[1];
+
 					if (format.equals("vhs")) {
-						for(int i = 0; i < Storage.vhs.size(); i++) {
+						for (int i = 0; i < Storage.vhs.size(); i++) {
 							Storage.vhs.get(i).setFormat(format);
 						}
 					}
-						
 				}
+				
 
 				if (line.contains("title")) {
 					String[] tokens = line.split(" ");
-					title = tokens[1];
-					Storage.vhs.get(0).setName(title);
+					if (tokens.length > 2) {
+						title = "";
+						for (int i = 1; i < tokens.length; i++)
+							title += tokens[i] + " ";
+					} else {
+						title = tokens[1];
+					}
+					newVHS = new VHSCollection(title);
+					Storage.vhs.add(newVHS);
 				}
 
 			}
