cc993f05882a38f8c00685508c4426a7629e8c5c
Travis Lawson <Travis.Lawson91@gmail.com>
2017-11-09 20:26:01 -0600

    rearranged and simplified some things in my main method test file

--- src/controller/MainTest.java
+++ src/controller/MainTest.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 import model.CDCollection;
 import model.DVDCollection;
+import model.LaserDiscCollection;
 import model.Storage;
 import model.VHSCollection;
 import model.Tools;
@@ -23,17 +24,26 @@ public class MainTest {
 		// Storage storedList = new Storage();
 		Scanner in = new Scanner(System.in);
 
-		Tools.readInFile();
-		for (int i = 0; i < Storage.vhs.size(); i++) {
-			System.out.println("Read in title: " + Storage.vhs.get(i).getName());
-			System.out.println("Read in format: " + Storage.vhs.get(i).getFormat());
-			System.out.println("Read in sleeve: " + Storage.vhs.get(i).getSleeveCondition());
+		System.out.print("Open vhs or dvd: ");
+		String open = in.nextLine();
+
+		if (open.equals("vhs")) {
+			Tools.readInFileVHS();
+			for (int i = 0; i < Storage.vhs.size(); i++) {
+				System.out.println("Read in title: " + Storage.vhs.get(i).getName());
+				System.out.println("Read in format: " + Storage.vhs.get(i).getFormat());
+				System.out.println("Read in sleeve: " + Storage.vhs.get(i).getSleeveCondition());
+			}
 		}
 
-		for (int i = 0; i < Storage.dvd.size(); i++) {
-			System.out.println("Read in title dvd: " + Storage.dvd.get(i).getName());
-			System.out.println("Read in format dvd: " + Storage.dvd.get(i).getFormat());
+		if(open.equals("dvd") || open.equals("DVD")){
+			Tools.readInFileDVD();
+			for (int i = 0; i < Storage.dvd.size(); i++) {
+				System.out.println("Read in title dvd: " + Storage.dvd.get(i).getName());
+				System.out.println("Read in format dvd: " + Storage.dvd.get(i).getFormat());
+			}
 		}
+		
 
 		boolean test = true;
 		String title;
@@ -44,13 +54,10 @@ public class MainTest {
 		String exit;
 		VHSCollection newVHS;
 		DVDCollection newDVD;
+		LaserDiscCollection newLD;
 		while (test) {
 			System.out.print("Title: ");
 			title = in.nextLine();
-			System.out.print("Director: ");
-			director = in.nextLine();
-			System.out.print("Year: ");
-			year = in.nextLine();
 			System.out.print("Format: ");
 			format = in.nextLine();
 			if (format.equals("vhs")) {
@@ -62,20 +69,29 @@ public class MainTest {
 					sleeve = " ";
 				newVHS.setSleeveCondition(sleeve);
 				Storage.vhs.add(newVHS);
+				Tools.writeFileVHS();
 			}
 
 			if (format.equals("dvd")) {
 				newDVD = new DVDCollection(title);
 				newDVD.setFormat(format);
 				Storage.dvd.add(newDVD);
+				Tools.writeFileDVD();
 			}
 
+			if (format.equals("laserdisc") || format.equals("ld")) {
+				newLD = new LaserDiscCollection(title);
+				newLD.setFormat(format);
+				Storage.ld.add(newLD);
+				Tools.writeFileLD();
+			}
+			
 			System.out.print("Exit: ");
 			exit = in.nextLine();
 			if (exit.equals("yes"))
 				test = false;
 			in.close();
-			Tools.writeFile();
+			System.out.println("Terminated");
 		}
 
 	}
