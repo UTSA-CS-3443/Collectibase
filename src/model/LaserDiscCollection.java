2cd1c4ef64d0fe30b2c20af853996b6f29388a21
Travis Lawson <Travis.Lawson91@gmail.com>
2017-11-09 20:28:26 -0600

    Chaged a few variables and getters/setters

--- src/model/LaserDiscCollection.java
+++ src/model/LaserDiscCollection.java
@@ -2,20 +2,29 @@ package model;
 
 public class LaserDiscCollection extends MoviesCollection {
 	
-	private String size;
+	private String encodingFormat; //CAV, CLV, CAA
+	private String region;
 	private String sides;
 	private String cover;
 	
-	public LaserDiscCollection(String title, String year) {
-		super(title, year);
+	public LaserDiscCollection(String title) {
+		super(title);
 	}
 
-	public String getSize() {
-		return size;
+	public String getEncodingFormat() {
+		return encodingFormat;
 	}
 
-	public void setSize(String size) {
-		this.size = size;
+	public void setEncodingFormat(String encodingFormat) {
+		this.encodingFormat = encodingFormat;
+	}
+	
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
 	}
 
 	public String getSides() {
