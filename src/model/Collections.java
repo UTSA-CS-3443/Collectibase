be1786467c127eda0926671935bfbfdc14240504
Travis Lawson <Travis.Lawson91@gmail.com>
2017-11-14 21:20:32 -0600

    removed rarity, value, and condition and moved the variables to TCGCollection.

--- src/model/Collections.java
+++ src/model/Collections.java
@@ -9,46 +9,20 @@ package model;
 
 public abstract class Collections {
 	/**
-	 * common traits that all collections share
+	 * Some common traits that all collections share
 	 */
 	private String name;
-	private String rarity;
 	private String genre;
-	private String condition;
 	private String year; 
-	private double value;
+	
 
 	//////// Getters and Setters////////
-	public String getCondition() {
-		return condition;
-	}
-
-	public void setCondition(String condition) {
-		this.condition = condition;
-	}
-
 	public String getGenre() {
 		return genre;
 	}
 
 	public void setGenre(String genre) {
 		this.genre = genre;
-	}
-
-	public double getValue() {
-		return value;
-	}
-
-	public void setValue(double value) {
-		this.value = value;
-	}
-
-	public String getRarity() {
-		return rarity;
-	}
-
-	public void setRarity(String rarity) {
-		this.rarity = rarity;
 	}
 
 	public String getName() {
