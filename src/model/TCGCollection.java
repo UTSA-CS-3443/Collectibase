4ea49058f5b0728a48cc997de974b26c4cd11293
Travis Lawson <Travis.Lawson91@gmail.com>
2017-11-14 21:24:56 -0600

    added additional variables, some from the Collections class

--- src/model/TCGCollection.java
+++ src/model/TCGCollection.java
@@ -2,8 +2,51 @@ package model;
 
 public class TCGCollection extends Collections {
 	
+	private String rarity;
+	private String condition;
+	private String cardGame; //which TCG: i.e. Pokemon, Magic, etc..
+	private double value;
+	
+	/**
+	 * Constructor for TCGCollection
+	 * @param name
+	 */
 	public TCGCollection(String name) {
 		setName(name);
 	}
+	
+	////////Getters and Setters\\\\\\\\
+	public String getRarity() {
+		return rarity;
+	}
+
+	public void setRarity(String rarity) {
+		this.rarity = rarity;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public String getCardGame() {
+		return cardGame;
+	}
+
+	public void setCardGame(String cardGame) {
+		this.cardGame = cardGame;
+	}
+
 
 }
