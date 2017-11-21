7f62316da768ceb80d24baef6f4ebcde2bb7b291
Jesus Nieto <wwh124@my.utsa.edu>
2017-11-21 10:06:02 -0600

    comments and deleting stuff from storage class

--- src/model/Storage.java
+++ src/model/Storage.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 import javafx.collections.ObservableList;
 
 /**
- * This purpose of the Storage class is to hold the Array Lists for each type of collection.
- * Since each array list is static, you are able to access each AL and the contents within it
- * through out the application by calling as "Tools.vhs.get(i).someMethodHere()"
+ * The purpose of the class is to hold temporary storage for the 
+ * collections when reading and writing into the file to keep the
+ * table views updated when accessing them again. 
  * 
+ * @author jesusnieto
  * @author Travis
  *
  */
