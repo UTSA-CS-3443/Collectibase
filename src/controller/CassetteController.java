34ad6e7ae80f7e8b79b77cc0e51356603d3183e1
Travis Lawson <Travis.Lawson91@gmail.com>
2017-12-03 03:41:59 -0600

    Squashed bug: table will now populate after loading into Cassette and after adding new entries to the table

--- src/controller/CassetteController.java
+++ src/controller/CassetteController.java
@@ -17,6 +17,16 @@ import model.CassetteCollection;
 import model.Storage;
 import view.CollectApp;
 
+/**
+ * 
+ * 
+ * @author Brenda Trejo
+ * debugged by:
+ * 
+ * @author Travis
+ * 			initialize was misspelled causing the cellValueFactory and getCassetteFromFile functions not to be called. 
+ *
+ */
 public class CassetteController {
 
 	@FXML
@@ -24,7 +34,7 @@ public class CassetteController {
 	public Button updateButton;
 	public Button deleteButton;
 	public Button insertButton;
-	
+
 	@FXML
 	public TextField artistField;
 	@FXML
@@ -59,12 +69,12 @@ public class CassetteController {
 	TableColumn<CassetteCollection, String> audioOutputColumn;
 	@FXML
 	TableColumn<CassetteCollection, String> endlessLoopColumn;
-	
-	public void intitialize() {
+
+	public void initialize() {
 		cellValueFactory();
 		getCassetteFromFile();
 	}
-	
+
 	public void backButtonHandle() {
 		CollectApp.stage.show();
 		CollectController.childScene.hide();
@@ -74,8 +84,8 @@ public class CassetteController {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-	} //backButtonHandle
-	
+	} // backButtonHandle
+
 	public void updateButtonHandle() {
 		Storage.allCassettes = cassTable.getItems();
 		try {
@@ -83,8 +93,8 @@ public class CassetteController {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-	} //updateButtonHandle
-	
+	} // updateButtonHandle
+
 	public void insertButtonHandle() {
 		CassetteCollection cassAdd = new CassetteCollection();
 		// Accounts for empty fields
@@ -93,50 +103,50 @@ public class CassetteController {
 		} else {
 			cassAdd.setArtist(artistField.getText());
 		}
-		
+
 		if (yearField.getText().equals("")) {
 			cassAdd.setYear("");
 		} else {
 			cassAdd.setYear(yearField.getText());
 		}
-		
+
 		if (albumField.getText().equals("")) {
 			cassAdd.setAlbum("");
 		} else {
 			cassAdd.setAlbum(albumField.getText());
 		}
-		
+
 		if (runningTimeField.getText().equals("")) {
 			cassAdd.setRunningTime("");
 		} else {
 			cassAdd.setRunningTime(runningTimeField.getText());
 		}
-		
+
 		if (typeField.getText().equals("")) {
 			cassAdd.setType("");
 		} else {
 			cassAdd.setType(typeField.getText());
 		}
-		
+
 		if (writeProtectionField.getText().equals("")) {
 			cassAdd.setWriteProtection("");
 		} else {
 			cassAdd.setWriteProtection(writeProtectionField.getText());
 		}
-		
+
 		if (audioOutputField.getText().equals("")) {
 			cassAdd.setAudioOutput("");
 		} else {
 			cassAdd.setAudioOutput(audioOutputField.getText());
-		} 
-		
+		}
+
 		if (endlessLoopField.getText().equals("")) {
 			cassAdd.setEndlessLoop("");
 		} else {
 			cassAdd.setEndlessLoop(endlessLoopField.getText());
 		}
-		
-		//Clear all fields after inserting a new item
+
+		// Clear all fields after inserting a new item
 		cassTable.getItems().add(cassAdd);
 		artistField.clear();
 		yearField.clear();
@@ -146,16 +156,16 @@ public class CassetteController {
 		writeProtectionField.clear();
 		audioOutputField.clear();
 		endlessLoopField.clear();
-		
+
 		// Writes to the cassette file to keep the table updated
 		Storage.allCassettes = cassTable.getItems();
 		try {
 			writeCassetteFile();
 		} catch (Exception ex) {
 			ex.printStackTrace();
-		}	
-	} //insertButtonHandle
-	
+		}
+	} // insertButtonHandle
+
 	public void deleteButtonHandle() {
 		ObservableList<CassetteCollection> cassSelected;
 		Storage.allCassettes = cassTable.getItems();
@@ -167,18 +177,18 @@ public class CassetteController {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-	} //deleteButtonHandle
-	
+	} // deleteButtonHandle
+
 	public void cellValueFactory() {
 		artistColumn.setCellValueFactory(new PropertyValueFactory<>("artist"));
-		yearColumn.setCellValueFactory(new PropertyValueFactory<>("yeavr"));
+		yearColumn.setCellValueFactory(new PropertyValueFactory<>("year"));
 		albumColumn.setCellValueFactory(new PropertyValueFactory<>("album"));
 		runningTimeColumn.setCellValueFactory(new PropertyValueFactory<>("runningTime"));
 		typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
 		writeProtectionColumn.setCellValueFactory(new PropertyValueFactory<>("writeProtection"));
 		audioOutputColumn.setCellValueFactory(new PropertyValueFactory<>("audioOutput"));
 		endlessLoopColumn.setCellValueFactory(new PropertyValueFactory<>("endlessLoop"));
-		
+
 		cassTable.setEditable(true);
 		artistColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 		yearColumn.setCellFactory(TextFieldTableCell.forTableColumn());
@@ -188,69 +198,76 @@ public class CassetteController {
 		writeProtectionColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 		audioOutputColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 		endlessLoopColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-	} //cellValueFactory
-	
+	} // cellValueFactory
+
 	/****
-	 * The methods below allow you to edit an item by double
-	 * clicking the cell. 
+	 * The methods below allow you to edit an item by double clicking the cell.
 	 */
 	public void changeArtist(CellEditEvent edditedCell) {
 		CassetteCollection artistSelected = cassTable.getSelectionModel().getSelectedItem();
 		artistSelected.setArtist(edditedCell.getNewValue().toString());
 	}
+
 	public void changeYear(CellEditEvent edditedCell) {
 		CassetteCollection yearSelected = cassTable.getSelectionModel().getSelectedItem();
 		yearSelected.setYear(edditedCell.getNewValue().toString());
 	}
+
 	public void changeAlbum(CellEditEvent edditedCell) {
 		CassetteCollection albumSelected = cassTable.getSelectionModel().getSelectedItem();
 		albumSelected.setAlbum(edditedCell.getNewValue().toString());
 	}
+
 	public void changeRunningTime(CellEditEvent edditedCell) {
 		CassetteCollection runningTimeSelected = cassTable.getSelectionModel().getSelectedItem();
 		runningTimeSelected.setRunningTime(edditedCell.getNewValue().toString());
 	}
+
 	public void changeType(CellEditEvent edditedCell) {
 		CassetteCollection typeSelected = cassTable.getSelectionModel().getSelectedItem();
 		typeSelected.setType(edditedCell.getNewValue().toString());
 	}
+
 	public void changeWriteProtection(CellEditEvent edditedCell) {
 		CassetteCollection writeProtectionSelected = cassTable.getSelectionModel().getSelectedItem();
 		writeProtectionSelected.setWriteProtection(edditedCell.getNewValue().toString());
 	}
+
 	public void changeAudioOutput(CellEditEvent edditedCell) {
 		CassetteCollection audioOutputSelected = cassTable.getSelectionModel().getSelectedItem();
 		audioOutputSelected.setAudioOutput(edditedCell.getNewValue().toString());
 	}
+
 	public void changeEndlessLoop(CellEditEvent edditedCell) {
 		CassetteCollection endlessLoopSelected = cassTable.getSelectionModel().getSelectedItem();
 		endlessLoopSelected.setEndlessLoop(edditedCell.getNewValue().toString());
 	}
-	
+
 	private void getCassetteFromFile() {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(new File("cassette.txt")));
 			String line;
 			String[] array;
-			while((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				array = line.split(",");
-				cassTable.getItems().add(new CassetteCollection(array[0], array[1], array[2], array[3], 
-						array[4], array[5], array[6], array[7]));
+				cassTable.getItems().add(new CassetteCollection(array[0], array[1], array[2], array[3], array[4],
+						array[5], array[6], array[7]));
 			}
 			br.close();
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-	} //getCassFromFile
-	
-	public void writeCassetteFile() throws Exception {
+	} // getCassFromFile
+
+	/* public */ private void writeCassetteFile() throws Exception {
 		Writer writer = null;
 		try {
 			File file = new File("cassette.txt");
 			writer = new BufferedWriter(new FileWriter(file));
 			for (CassetteCollection cass : Storage.allCassettes) {
-				String text = cass.getArtist() + "," + cass.getYear() + "," + cass.getAlbum() + "," + cass.getRunningTime() + "," +
-							cass.getType() + "," + cass.getWriteProtection() + "," + cass.getAudioOutput() + "," + cass.getEndlessLoop() + "\n";
+				String text = cass.getArtist() + "," + cass.getYear() + "," + cass.getAlbum() + ","
+						+ cass.getRunningTime() + "," + cass.getType() + "," + cass.getWriteProtection() + ","
+						+ cass.getAudioOutput() + "," + cass.getEndlessLoop() + "\n";
 				writer.write(text);
 			}
 		} catch (Exception ex) {
@@ -259,5 +276,6 @@ public class CassetteController {
 			writer.flush();
 			writer.close();
 		}
-	} //writeCassFile
-} //CassetteController
+	} // writeCassFile
+
+} // CassetteController
